6cd2c1317408b74b9b8387ec880d69ff972640fc
Svetliyan Spasov <133858086+Svetliyan@users.noreply.github.com>
2025-03-20 10:59:20 +0200

    Get Game Page and Get Game By Id

--- GameLibrary/src/main/java/app/web/GameController.java
+++ GameLibrary/src/main/java/app/web/GameController.java
@@ -2,20 +2,17 @@ package app.web;
 
 import app.category.model.Category;
 import app.category.service.CategoryService;
+import app.game.model.Game;
 import app.game.service.GameService;
 import app.user.model.User;
 import app.user.service.UserService;
-import app.web.dto.CreateCategoryRequest;
 import app.web.dto.CreateGameRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -66,5 +63,27 @@ public class GameController {
         gameService.createGame(createGameRequest, user);
 
         return "redirect:/store";
+    }
+
+    @GetMapping("games/{id}")
+    public ModelAndView getGamePage(@PathVariable UUID id, HttpSession session) {
+        UUID userId = (UUID) session.getAttribute("user_id");
+        User user = userService.getById(userId);
+
+        Game game =  gameService.getById(id);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("games");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView getGameById(@PathVariable UUID id) {
+        Game game = gameService.getById(id);
+
+        ModelAndView modelAndView = new ModelAndView("games");
+        modelAndView.addObject("game", game);
+
+        return modelAndView;
     }
 }
