4c6791cc65d46bc725ca23e0b6ff559e7ab996ea
Svetliyan Spasov <133858086+Svetliyan@users.noreply.github.com>
2025-03-25 22:44:49 +0200

    Only admin can now create new gift cards

--- GameLibrary/src/main/java/app/web/GiftCardRestController.java
+++ GameLibrary/src/main/java/app/web/GiftCardRestController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -69,6 +70,7 @@ public class GiftCardRestController {
 
     // Създаване на нова подаръчна карта
     @PostMapping("/add")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> createNewGiftCard(@RequestBody @Valid CreateGiftCardRequest createGiftCardRequest,
                                                BindingResult bindingResult,
                                                @AuthenticationPrincipal AuthenticationDetails authenticationDetails) {
